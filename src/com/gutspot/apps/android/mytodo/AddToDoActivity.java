24c89aaf4a34b1c967229766a47d25261433f77c
mingshun <gutspot@qq.com>
2014-06-29 21:09:52 +0800

    Added logics to liesteners of color buttons

--- src/com/gutspot/apps/android/mytodo/AddToDoActivity.java
+++ src/com/gutspot/apps/android/mytodo/AddToDoActivity.java
@@ -8,10 +8,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class AddToDoActivity extends Activity {
+
+    private int pressedTextColorButtonId = R.id.button_text_color_black;
+    private int pressedBackgroundButtonId = R.id.button_background_color_yellow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,22 +24,8 @@ public class AddToDoActivity extends Activity {
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        int[] textColorButtonIds = new int[] { R.id.button_text_color_black, R.id.button_text_color_red,
-                R.id.button_text_color_green, R.id.button_text_color_blue };
-        OnClickListener changeTextColorListener = new ChangeTextColorListener();
-        for (int textColorButtonId : textColorButtonIds) {
-            Button textColorButton = (Button) this.findViewById(textColorButtonId);
-            textColorButton.setOnClickListener(changeTextColorListener);
-        }
-
-        int[] backgroundButtonIds = new int[] { R.id.button_background_color_yellow,
-                R.id.button_background_color_orage, R.id.button_background_color_red,
-                R.id.button_background_color_green, R.id.button_background_color_blue };
-        OnClickListener changeBackgroundColorListener = new ChangeBackgroundColorListener();
-        for (int backgroundButtonId : backgroundButtonIds) {
-            Button backgroundButton = (Button) this.findViewById(backgroundButtonId);
-            backgroundButton.setOnClickListener(changeBackgroundColorListener);
-        }
+        setColorButtonListeners();
+        initPressedColorButton();
     }
 
     @Override
@@ -67,25 +56,88 @@ public class AddToDoActivity extends Activity {
 
     }
 
+    private void setColorButtonListeners() {
+        int[] textColorButtonIds = new int[] { R.id.button_text_color_black, R.id.button_text_color_red,
+                R.id.button_text_color_green, R.id.button_text_color_blue };
+        OnClickListener changeTextColorListener = new ChangeTextColorListener(textColorButtonIds);
+        for (int textColorButtonId : textColorButtonIds) {
+            ImageButton textColorButton = (ImageButton) this.findViewById(textColorButtonId);
+            textColorButton.setOnClickListener(changeTextColorListener);
+        }
+
+        int[] backgroundButtonIds = new int[] { R.id.button_background_color_yellow,
+                R.id.button_background_color_orage, R.id.button_background_color_red,
+                R.id.button_background_color_green, R.id.button_background_color_blue };
+        OnClickListener changeBackgroundColorListener = new ChangeBackgroundColorListener(backgroundButtonIds);
+        for (int backgroundButtonId : backgroundButtonIds) {
+            ImageButton backgroundButton = (ImageButton) this.findViewById(backgroundButtonId);
+            backgroundButton.setOnClickListener(changeBackgroundColorListener);
+        }
+    }
+
+    private void initPressedColorButton() {
+        ImageButton pressedTextColorButton = (ImageButton) this.findViewById(pressedTextColorButtonId);
+        pressedTextColorButton.setImageResource(R.drawable.ic_action_accept);
+        ColorDrawable textColor = (ColorDrawable) pressedTextColorButton.getBackground();
+
+        ImageButton pressedBackgroundColorButton = (ImageButton) this.findViewById(pressedBackgroundButtonId);
+        pressedBackgroundColorButton.setImageResource(R.drawable.ic_action_accept_light);
+        ColorDrawable backgroundColor = (ColorDrawable) pressedBackgroundColorButton.getBackground();
+
+        EditText edit = (EditText) AddToDoActivity.this.findViewById(R.id.edit_content);
+        edit.setTextColor(textColor.getColor());
+        edit.setBackgroundColor(backgroundColor.getColor());
+    }
+
     class ChangeTextColorListener implements OnClickListener {
+
+        private int[] buttonIds;
+
+        public ChangeTextColorListener(int[] buttonIds) {
+            this.buttonIds = buttonIds;
+        }
 
         @Override
         public void onClick(View v) {
-            Button button = (Button) v;
+            ImageButton button = (ImageButton) v;
+            button.setImageResource(R.drawable.ic_action_accept);
+            for (int buttonId : buttonIds) {
+                if (button.getId() != buttonId) {
+                    ImageButton b = (ImageButton) AddToDoActivity.this.findViewById(buttonId);
+                    b.setImageResource(android.R.color.transparent);
+                }
+            }
+
             ColorDrawable color = (ColorDrawable) button.getBackground();
             EditText edit = (EditText) AddToDoActivity.this.findViewById(R.id.edit_content);
             edit.setTextColor(color.getColor());
+            pressedTextColorButtonId = button.getId();
         }
     }
 
     class ChangeBackgroundColorListener implements OnClickListener {
 
+        private int[] buttonIds;
+
+        public ChangeBackgroundColorListener(int[] buttonIds) {
+            this.buttonIds = buttonIds;
+        }
+
         @Override
         public void onClick(View v) {
-            Button button = (Button) v;
+            ImageButton button = (ImageButton) v;
+            button.setImageResource(R.drawable.ic_action_accept_light);
+            for (int buttonId : buttonIds) {
+                if (button.getId() != buttonId) {
+                    ImageButton b = (ImageButton) AddToDoActivity.this.findViewById(buttonId);
+                    b.setImageResource(android.R.color.transparent);
+                }
+            }
+            
             ColorDrawable color = (ColorDrawable) button.getBackground();
             EditText edit = (EditText) AddToDoActivity.this.findViewById(R.id.edit_content);
             edit.setBackgroundColor(color.getColor());
+            pressedBackgroundButtonId = button.getId();
         }
 
     }
