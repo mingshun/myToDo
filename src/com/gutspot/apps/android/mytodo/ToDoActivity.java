6b0950a478c47395cf69a8ed5b76615be42d654c
mingshun <gutspot@qq.com>
2014-07-07 17:46:55 +0800

    Added long click options menu to memo items

--- src/com/gutspot/apps/android/mytodo/ToDoActivity.java
+++ src/com/gutspot/apps/android/mytodo/ToDoActivity.java
@@ -3,6 +3,12 @@ package com.gutspot.apps.android.mytodo;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,16 +16,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.gutspot.apps.android.mytodo.adapter.MemoAdapter;
 import com.gutspot.apps.android.mytodo.dao.MemoDAO;
 import com.gutspot.apps.android.mytodo.model.Memo;
+import com.gutspot.apps.android.mytodo.utils.AlertUtil;
 
 public class ToDoActivity extends Activity {
 
     private long toDoId;
+
+    private ListView memoListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +44,8 @@ public class ToDoActivity extends Activity {
             return;
         }
 
-        initMemoList();
+        memoListView = (ListView) this.findViewById(R.id.list_memo);
+        updateMemoListView();
     }
 
     @Override
@@ -68,11 +79,11 @@ public class ToDoActivity extends Activity {
         }
     }
 
-    private void initMemoList() {
+    private void updateMemoListView() {
         MemoDAO memoDAO = new MemoDAO(this);
         List<Memo> memos = memoDAO.findByToDoId(toDoId);
         final MemoAdapter adapter = new MemoAdapter(this, memos);
-        ListView memoListView = (ListView) this.findViewById(R.id.list_memo);
+
         memoListView.setAdapter(adapter);
         memoListView.setOnItemClickListener(new OnItemClickListener() {
 
@@ -87,6 +98,90 @@ public class ToDoActivity extends Activity {
                 activity.startActivity(intent);
             }
         });
+        memoListView.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Memo memo = (Memo) adapter.getItem(position);
+                DialogFragment optionsDialog = MemoOptionsDialog.newInstance(ToDoActivity.this, toDoId, memo.getId());
+                optionsDialog.show(ToDoActivity.this.getFragmentManager(), "dialog");
+                return false;
+            }
+        });
+    }
+
+    public static class MemoOptionsDialog extends DialogFragment implements OnClickListener {
+        private static final String[] options = new String[] { "编辑", "删除" };
+
+        private Context context;
+        private long toDoId;
+        private long memoId;
+
+        public static MemoOptionsDialog newInstance(Context context, long toDoId, long memoId) {
+            MemoOptionsDialog dialog = new MemoOptionsDialog();
+            dialog.context = context;
+            dialog.toDoId = toDoId;
+            dialog.memoId = memoId;
+            return dialog;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("提示");
+            builder.setItems(options, this);
+            return builder.create();
+        }
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+            case 0:
+                viewMemo();
+                break;
+
+            case 1:
+                deleteMemo();
+                break;
+
+            default:
+
+            }
+        }
+
+        private void viewMemo() {
+            Intent intent = new Intent(context, MemoActivity.class);
+            intent.putExtra("type", 3);
+            intent.putExtra("todo_id", toDoId);
+            intent.putExtra("memo_id", memoId);
+            context.startActivity(intent);
+        }
+
+        private void deleteMemo() {
+            String message = "是否删除Memo？";
+
+            String yesLabel = "是";
+            DialogInterface.OnClickListener yesListener = new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    MemoDAO memoDAO = new MemoDAO(context);
+                    memoDAO.remove(memoId);
+                    ((ToDoActivity) context).updateMemoListView();
+                }
+            };
+
+            String noLabel = "否";
+            DialogInterface.OnClickListener noListener = new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            };
+
+            AlertUtil.show(context, message, yesLabel, yesListener, noLabel, noListener);
+        }
     }
 
 }
