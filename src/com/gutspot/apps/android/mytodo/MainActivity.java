8f5b74639fee98529d144f45c042d568f12ee9bd
mingshun <gutspot@qq.com>
2014-07-07 17:23:59 +0800

    Added long click options menu to todo items

--- src/com/gutspot/apps/android/mytodo/MainActivity.java
+++ src/com/gutspot/apps/android/mytodo/MainActivity.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,6 +20,7 @@ import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -24,6 +31,7 @@ import com.gutspot.apps.android.mytodo.dao.MemoDAO;
 import com.gutspot.apps.android.mytodo.dao.ToDoDAO;
 import com.gutspot.apps.android.mytodo.model.Memo;
 import com.gutspot.apps.android.mytodo.model.ToDo;
+import com.gutspot.apps.android.mytodo.utils.AlertUtil;
 
 public class MainActivity extends Activity implements OnTouchListener {
 
@@ -194,6 +202,86 @@ public class MainActivity extends Activity implements OnTouchListener {
                 activity.startActivity(intent);
             }
         });
+        toDoListView.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                ToDoItem item = (ToDoItem) adapter.getItem(position);
+                DialogFragment optionsDialog = ToDoOptionsDialog.newInstance(MainActivity.this, item.id);
+                optionsDialog.show(MainActivity.this.getFragmentManager(), "dialog");
+                return false;
+            }
+
+        });
     }
 
+    public static class ToDoOptionsDialog extends DialogFragment implements OnClickListener {
+        private static final String[] options = new String[] { "查看", "删除" };
+
+        private Context context;
+        private long toDoId;
+
+        public static ToDoOptionsDialog newInstance(Context context, long toDoId) {
+            ToDoOptionsDialog dialog = new ToDoOptionsDialog();
+            dialog.context = context;
+            dialog.toDoId = toDoId;
+            return dialog;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("提示");
+            builder.setItems(options, this);
+            return builder.create();
+        }
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+            case 0:
+                viewToDo();
+                break;
+
+            case 1:
+                deleteToDo();
+                break;
+
+            default:
+
+            }
+        }
+
+        private void viewToDo() {
+            Intent intent = new Intent(context, ToDoActivity.class);
+            intent.putExtra("todo_id", toDoId);
+            context.startActivity(intent);
+        }
+
+        private void deleteToDo() {
+            String message = "是否删除ToDo？";
+
+            String yesLabel = "是";
+            DialogInterface.OnClickListener yesListener = new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ToDoDAO toDoDAO = new ToDoDAO(context);
+                    toDoDAO.remove(toDoId);
+                    ((MainActivity) context).updateToDoListView();
+                }
+            };
+
+            String noLabel = "否";
+            DialogInterface.OnClickListener noListener = new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            };
+
+            AlertUtil.show(context, message, yesLabel, yesListener, noLabel, noListener);
+        }
+    }
 }
